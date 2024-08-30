5532553f77a0f228d169798fff1ecf84c4a13d4f
fmonroy75 <83427525+fmonroy75@users.noreply.github.com>
2024-08-29 20:17:16 -0400

    Update tienda.py
    clase restaurant

--- tienda.py
+++ tienda.py
@@ -1,3 +1,4 @@
+import producto
 from abc import ABC, abstractmethod
 
 class Tienda(ABC):
@@ -25,3 +26,35 @@ class Tienda(ABC):
     def realizar_venta(self):
         pass
     
+    
+class Restaurante(Tienda):
+    def ingresar_producto(self):
+        nombre = input("Ingrese el nombre del producto: ")
+        precio = float(input("Ingrese el precio del producto: "))
+        producto = producto(nombre, precio, 0)
+
+        for prod in self._lista_productos:
+            if prod == producto:
+                print("El producto ya existe y no se modifica el stock.")
+                return
+
+        self._lista_productos.append(producto)
+        print("Producto ingresado exitosamente.")
+
+    def listar_productos(self):
+        cadena="" 
+        for prod in self._lista_productos
+            cadena=cadena + "\n" +"Producto: " + prod.nombre +", Precio: $" + prod.precio 
+        return cadena
+        
+    def realizar_venta(self):
+        nombre = input("Ingrese el nombre del producto a vender: ")
+        cantidad = int(input("Ingrese la cantidad a vender: "))
+
+        for prod in self._lista_productos:
+            if prod.nombre == nombre:
+                print("Venta realizada.")
+                return
+        print("Producto no encontrado.")
+
+
