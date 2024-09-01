fc08bea9a174a66ed9cca1769f10a811f407fee8
Natalia <90008571+StudentNPD@users.noreply.github.com>
2024-09-01 00:34:33 -0400

    Funciones agregadas, seleccionar tipo de tienda, ingresar producto, listar producto y realizar venta

--- programa.py
+++ programa.py
@@ -1,25 +1,32 @@
-#from producto import Producto
-#from tienda import Tienda
+from tienda import Tienda, Farmacia, Restaurante #,Supermercado
 
-#INSTANCIAR LA CLASE TIENDA
-#mi_tienda = Tienda(tipo,nombre,precio)
+tipo_tienda = int(input("Ingrese el tipo de tienda: \n1)Supermercado\n2)Farmacia\n3)Restaurante\n"))
+nombre_tienda = input("Ingrese el nombre de la tienda:\n")
+delivery = input("Ingrese costo de delivery:\n")
+
+mi_tienda = Tienda
+
+if tipo_tienda == 1:
+    print("Supermercado")
+    #mi_tienda = Supermercado(nombre_tienda,delivery)
+elif tipo_tienda == 2:
+    mi_tienda = Farmacia(nombre_tienda,delivery)
+elif tipo_tienda == 3:
+    mi_tienda = Restaurante(nombre_tienda,delivery)
+else: 
+    print("Por favor ingrese la opción: 1,2 o 3")
 
 while True:
     print("Menu: \n1)Listar Productos\n2)Ingresar Producto\n3)Realizar Venta\n4)Salir\n")
     opcion = int(input("Ingrese Opcion\n"))
     if opcion == 1:
-        print("Listando Productos\n")
+        print(mi_tienda.listar_productos())
     elif opcion == 2:
-        print("Ingresar Producto\n")
+        mi_tienda.ingresar_producto()
     elif opcion == 3:
-        print("Realizar venta\n")
+        mi_tienda.realizar_venta()
     elif opcion == 4:
         print("Gracias por ingresar a la tienda!\n")
         break
     else:
         print("Ingrese una opcón válida\n")
-
-    #producto_nuevo = input("Ingrese un producto").title()
-    
-    
-    
