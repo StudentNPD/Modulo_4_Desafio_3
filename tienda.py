45c5e44c9ed8a664462df4fa40cd2185e97081a0
Francisco Colomer B. <francisco.colomer@gmail.com>
2024-09-02 09:29:41 -0400

    Update tienda.py
    Se agregan DocStrings

--- tienda.py
+++ tienda.py
@@ -1,37 +1,80 @@
-from producto import Producto # agregado
+from producto import Producto  # agregado
 from abc import ABC, abstractmethod
 
 class Tienda(ABC):
     def __init__(self, nombre, costo_delivery):
+        """
+        Inicializa una instancia de la clase Tienda.
+
+        Args:
+            nombre (str): El nombre de la tienda.
+            costo_delivery (float): El costo del servicio de entrega.
+        """
         self._nombre = nombre
         self._costo_delivery = costo_delivery
         self._lista_productos = []
 
     @property
     def nombre(self):
+        """
+        Obtiene el nombre de la tienda.
+
+        Returns:
+            str: El nombre de la tienda.
+        """
         return self._nombre
 
     @property
     def costo_delivery(self):
+        """
+        Obtiene el costo del servicio de entrega.
+
+        Returns:
+            float: El costo de delivery.
+        """
         return self._costo_delivery
 
     @abstractmethod
     def ingresar_producto(self):
+        """
+        Método abstracto para ingresar un producto a la tienda.
+        """
         pass
+
     @abstractmethod
     def listar_productos(self):
+        """
+        Método abstracto para listar los productos disponibles en la tienda.
+
+        Returns:
+            str: Una cadena con la lista de productos y sus detalles.
+        """
         pass
 
     @abstractmethod
     def realizar_venta(self):
+        """
+        Método abstracto para realizar una venta de un producto.
+
+        Returns:
+            None
+        """
         pass
-    
-    
+
+
 class Restaurante(Tienda):
     def ingresar_producto(self):
+        """
+        Ingresa un nuevo producto al restaurante.
+        
+        Solicita al usuario el nombre y el precio del producto, y lo añade a la lista de productos si no existe ya.
+
+        Returns:
+            None
+        """
         nombre = input("Ingrese el nombre del producto: ")
         precio = float(input("Ingrese el precio del producto: "))
-        producto = Producto(nombre, precio, 0) # producto estaba en  minuscula y por eso no lo tomaba
+        producto = Producto(nombre, precio, 0)
 
         for prod in self._lista_productos:
             if prod == producto:
@@ -42,12 +85,26 @@ class Restaurante(Tienda):
         print("Producto ingresado exitosamente.")
 
     def listar_productos(self):
-        cadena="" 
+        """
+        Lista los productos disponibles en el restaurante.
+
+        Returns:
+            str: Una cadena que contiene la lista de productos con sus nombres y precios.
+        """
+        cadena = "" 
         for prod in self._lista_productos:
-            cadena=cadena + "\n" +"Producto: " + prod.nombre +", Precio: $" + str(prod.precio)  #prod.precio. No concatena por que es un int 
+            cadena = cadena + "\n" + "Producto: " + prod.nombre + ", Precio: $" + str(prod.precio)
         return cadena
         
     def realizar_venta(self):
+        """
+        Realiza una venta de un producto del restaurante.
+
+        Solicita al usuario el nombre del producto y la cantidad a vender, y realiza la venta si el producto existe.
+
+        Returns:
+            None
+        """
         nombre = input("Ingrese el nombre del producto a vender: ")
         cantidad = int(input("Ingrese la cantidad a vender: "))
 
@@ -57,12 +114,22 @@ class Restaurante(Tienda):
                 return
         print("Producto no encontrado.")
 
+
 class Farmacia(Tienda):
     def ingresar_producto(self):
+        """
+        Ingresa un nuevo producto a la farmacia.
+        
+        Solicita al usuario el nombre, precio y stock del producto, y lo añade a la lista de productos si no existe ya.
+        Si el producto existe, se actualiza su stock.
+
+        Returns:
+            None
+        """
         nombre = input("Ingrese el nombre del producto: ")
         precio = float(input("Ingrese el precio del producto: "))
         stock = int(input("Ingrese el stock del producto: "))
-        producto = Producto(nombre, precio, stock) # producto estaba en  minuscula y por eso no lo tomaba
+        producto = Producto(nombre, precio, stock)
 
         for prod in self._lista_productos:
             if prod == producto:
@@ -74,16 +141,28 @@ class Farmacia(Tienda):
         print("Producto ingresado exitosamente.")
 
     def listar_productos(self):
-        cadena="" 
+        """
+        Lista los productos disponibles en la farmacia.
+
+        Returns:
+            str: Una cadena que contiene la lista de productos con sus nombres, precios y posibles beneficios de envío.
+        """
+        cadena = "" 
         for prod in self._lista_productos:
-            cadena=cadena + "\n" +"Producto: " + prod.nombre +", Precio: $" + str(prod.precio)  #prod.precio. No concatena por que es un int 
+            cadena = cadena + "\n" + "Producto: " + prod.nombre + ", Precio: $" + str(prod.precio)
             if prod.precio > 15000:
-                cadena=cadena + '(Envío gratis al solicitar este producto) '
-            else:
-                cadena=cadena +''
+                cadena = cadena + '(Envío gratis al solicitar este producto) '
         return cadena
 
     def realizar_venta(self):
+        """
+        Realiza una venta de un producto de la farmacia.
+
+        Solicita al usuario el nombre del producto y la cantidad a vender. Si la cantidad solicitada es mayor a 3 o el stock es insuficiente, la venta no se realiza.
+
+        Returns:
+            None
+        """
         nombre = input("Ingrese el nombre del producto a vender: ")
         cantidad = int(input("Ingrese la cantidad a vender: "))
 
@@ -92,8 +171,8 @@ class Farmacia(Tienda):
             return
 
         for prod in self._lista_productos:
-            if prod.get_nombre() == nombre:  # nombre sin el get no ocupa los getter de producto.py
-                vendida = prod.get_stock() - cantidad # prod sin el get no ocupa los getter de producto.py
+            if prod.get_nombre() == nombre:
+                vendida = prod.get_stock() - cantidad
                 if vendida > 0:
                     print(f"Venta realizada. Cantidad vendida: {vendida}")
                 else:
@@ -102,13 +181,21 @@ class Farmacia(Tienda):
         print("Producto no encontrado.")
 
 
-
 class Supermercado(Tienda):
     def ingresar_producto(self):
+        """
+        Ingresa un nuevo producto al supermercado.
+        
+        Solicita al usuario el nombre, precio y stock del producto, y lo añade a la lista de productos si no existe ya.
+        Si el producto existe, se actualiza su stock.
+
+        Returns:
+            None
+        """
         nombre = input("Ingrese el nombre del producto: ")
         precio = float(input("Ingrese el precio del producto: "))
         stock = int(input("Ingrese el stock del producto: "))
-        producto = producto(nombre, precio, stock)
+        producto = Producto(nombre, precio, stock)
 
         for prod in self._lista_productos:
             if prod == producto:
@@ -120,25 +207,38 @@ class Supermercado(Tienda):
         print("Producto ingresado exitosamente.")
 
     def listar_productos(self):
-        cadena="" 
+        """
+        Lista los productos disponibles en el supermercado.
+
+        Returns:
+            str: Una cadena que contiene la lista de productos con sus nombres, precios, stock, y un aviso si el stock es bajo.
+        """
+        cadena = "" 
         for prod in self._lista_productos:
-            cadena=cadena + "\n" +"Producto: " + prod.nombre +", Precio: $" + str(prod.precio) +", Stock: " + int(prod.stock) 
-            if prod.stock < 10 :
-                cadena=cadena + '(Pocos productos disponibles)'
-            else:
-                cadena=cadena +''
+            cadena = cadena + "\n" + "Producto: " + prod.nombre + ", Precio: $" + str(prod.precio) + ", Stock: " + str(prod.stock)
+            if prod.stock < 10:
+                cadena = cadena + '(Pocos productos disponibles)'
         return cadena
         
     def realizar_venta(self):
+        """
+        Realiza una venta de un producto del supermercado.
+
+        Solicita al usuario el nombre del producto y la cantidad a vender. Si el stock es insuficiente, la venta no se realiza.
+
+        Returns:
+            None
+        """
         nombre = input("Ingrese el nombre del producto a vender: ")
         cantidad = int(input("Ingrese la cantidad a vender: "))
 
         for prod in self._lista_productos:
             if prod.nombre == nombre:
-                vendida = prod - cantidad
+                vendida = prod.stock - cantidad
                 if vendida > 0:
                     print(f"Venta realizada. Cantidad vendida: {vendida}")
                 else:
                     print("Stock insuficiente.")
                 return
         print("Producto no encontrado.")
+
